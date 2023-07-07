bf7ef8efdd385e2d22795d9e686ecd0a2dfee699
Ashish <ashish19.singh@paytm.com>
2023-07-07 17:51:20 +0530

    Auth using next auth and google OAuth is done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
+import Navbar from '@/components/Navbar'
+import { Toaster } from '@/components/ui/Toaster'
+import { cn } from '@/lib/utils'
 import '@/styles/globals.css'
+import {Inter} from 'next/font/google'
 
 export const metadata = {
-  title: 'Breadit',
-  description: 'A Reddit clone built with Next.js and TypeScript.',
+  title: 'ThreadIt',
+  description: 'Reddit clone built with Next.js and TypeScript for communities!',
 }
+
+const inter = Inter({subsets: ['latin']})
+
+
 
 export default function RootLayout({
   children,
@@ -11,8 +19,17 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang='en'>
-      <body>{children}</body>
+    <html lang='en'
+      className={cn('bg-white text-slate-900 antialiased light', inter.className)}
+    >
+      <body className='min-h-screen pt-12 bg-slate-50 antialiased'>
+        <Navbar />
+        <div className='container max-w-7xl mx-auto h-full pt-12'>
+        {children}
+        </div>
+
+        <Toaster />
+      </body>
     </html>
   )
 }
